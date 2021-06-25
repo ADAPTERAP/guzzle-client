c2b15316897536db72d9a4d004056baff4c9ced9
Nikita Kozlovskii <nikita@adapter.ru>
2021-06-25 14:36:17 +0300

    добавлено значение по умолчанию и обновлена версия

--- src/GuzzleClientResponse.php
+++ src/GuzzleClientResponse.php
@@ -225,7 +225,7 @@ class GuzzleClientResponse implements ResponseInterface
      * @throws ForbiddenException            On a 403 when $throw is true
      * @throws ServerExceptionInterface      On a 5xx when $throw is true
      */
-    public function throwAnExceptionIfNeed(bool $throw): void
+    public function throwAnExceptionIfNeed(bool $throw = true): void
     {
         if ($throw && $this->getStatusCode() >= Response::HTTP_INTERNAL_SERVER_ERROR) {
             throw new ServerException($this);
